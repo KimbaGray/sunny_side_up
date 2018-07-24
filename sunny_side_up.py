f663f9b907b3824e2b838802ae7f7a295eb2f5fd
Kim <kim-gray@hotmail.co.uk>
2018-07-24 23:43:30 +0100

    use bulma

--- sunny_side_up.py
+++ sunny_side_up.py
@@ -3,16 +3,15 @@ from datetime import datetime
 
 app = Flask("MyApp")
 
-#Default route this method will be called when you hit http://127.0.0.0:5000/
 @app.route("/")
 def home():
-	return render_template("homepage.html") # render_template method is a special function flask which redirect to the html file mentioned in the paramter
+	return render_template("homepage.html")
 
 @app.route("/search_postcode", methods=["POST"])
 def read_label_data():
-	label_data = request.label #Getting hold of a Form object that is sent from a browser.
-	calculate_location =  label_data["location"] # from the form object getting value of dob field.
-	location= calculate_location(area) #Calling internal method which takes year as a paramter and return text
+	form_data = request.form
+	location = form_data.get("location")
+	return render_template("restaurant_page.html", location=location)
 
 
 if __name__ == "__main__":
