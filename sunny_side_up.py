b4e7fe52a654b139ce7e6c3a11b6898eec747e9c
Kim <kim-gray@hotmail.co.uk>
2018-07-24 18:26:19 +0100

    change python

--- sunny_side_up.py
+++ sunny_side_up.py
@@ -8,6 +8,12 @@ app = Flask("MyApp")
 def home():
 	return render_template("homepage.html") # render_template method is a special function flask which redirect to the html file mentioned in the paramter
 
+@app.route("/home", methods=["POST"])
+def read_label_data():
+	label_data = request.label #Getting hold of a Form object that is sent from a browser.
+	calculate_location =  label_data["location"] # from the form object getting value of dob field.
+	location= calculate_location(area) #Calling internal method which takes year as a paramter and return text
+
 #Default route this method will be called when you hit http://127.0.0.0:5000/
 @app.route("/filterpage")
 def filterpage():
