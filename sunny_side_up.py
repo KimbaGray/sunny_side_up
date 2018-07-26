6155d0f5f50509522238350c046b9ff439f859e0
Kim <kim-gray@hotmail.co.uk>
2018-07-26 14:13:17 +0100

    change logo

--- sunny_side_up.py
+++ sunny_side_up.py
@@ -12,11 +12,6 @@ def search_area():
 	location = request.form["location"]
 	return render_template("north.html", location=location)
 
-@app.route("/homepage", methods=["POST"])
-def search_area():
-	return render_template("homepage.html")
-
-
 	# form_data = request.form
 	# location = form_data.get("location")
 	# return render_template("restaurant_page.html", location=location)
